6974a430a0dec2b5c2b911584ef9ae1c4a6e3e64
Frassinelli Marco <fras.marco@gmail.com>
2017-08-07 17:53:03 +0200

    Setup postgres

--- setup.sh
+++ setup.sh
@@ -15,6 +15,16 @@ sudo apt-get install nodejs
 sudo certbot --apache
 
 # Enable the reverse proxy
-a2enmod proxy_http
+sudo a2enmod proxy_http
 sudo cp proxy.conf /etc/apache2/mods-enabled/proxy.conf
 sudo systemctl restart apache2
+
+# Clone ApiServer
+git clone https://github.com/frasmarco/apiServer.git
+cd apiServer
+npm install
+## Remember to configure it!
+
+echo "host    all             all             all            md5" >> /etc/postgresql/9.6/main/pg_hba.conf
+cp postgresql.conf /etc/postgresql/9.6/main/
+chown posgres:postgres /etc/postgresql/9.6/main/postgresql.conf
